832724fd5872086ada4aa8cdebc28ad853e90eb3
frisco lieyanto <friscolieyanto@gmail.com>
2021-07-02 21:56:16 +0700

    adding aos and particle

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,15 @@ module.exports = {
         darkblue: "#0C0217",
         darkerblue: "#0e1130",
       },
+      boxShadow: {
+        new: "0 20px 25px -5px rgba(54, 212, 201,0.7), 0 10px 10px -5px rgba(54, 212, 201, 0.05)",
+      },
+      borderRadius: {
+        full: "4,5rem",
+      },
+      zIndex: {
+        "-1": "-1",
+      },
     },
   },
   variants: {
