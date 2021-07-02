1c02f3188ae8c04359f7e918023ba3d7a69adb9d
frisco lieyanto <friscolieyanto@gmail.com>
2021-07-02 17:09:39 +0700

    commit-v1

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ["./src/**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        darkblue: "#0C0217",
+        darkerblue: "#0e1130",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
